114c69958cdf83586162288b11257fce34c76c61
Iuliia Botukh <u.botuh@yandex.ru>
2025-07-24 15:28:27 +0400

    14

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
 from settings import Config
+from .exceptions import URLMapException
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+@app.errorhandler(URLMapException)
+def handle_url_map_exception(error):
+    return jsonify({'message': error.message}), error.status_code
+
 
 from . import api_views, error_handlers, forms, views, ydisk, forms
